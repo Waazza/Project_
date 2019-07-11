b783d5896017e5f767a0283e820296b5383609aa
Loick <loickbriand@gmail.com>
2019-07-11 12:08:15 +0200

    Mon-compte + fav + test Mapbox

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src='https://api.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css' rel='stylesheet' />
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Project</title>
 </head>
--- resources/views/mon-compte/dashboard.blade.php
+++ resources/views/mon-compte/dashboard.blade.php
@@ -7,7 +7,43 @@
     </div>
     <div class="row dash-row">
         <div class="col-6">
-            <h2 class="text-center">Vos Favoris</h2>
+            <h2 class="text-center mb-5">Vos Favoris</h2>
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                            <div class="col-md-4 img-trap">
+                                <img src="/storage/cat.jpg" class="card-img img-fluid img-thumb" alt="">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">*Nom de l'animal*</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet asperiores aspernatur at consequatur dolorum eaque illo ipsa laboriosam.</p>
+                                    <a href="#" class="card-link">Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                            <div class="col-md-4 img-trap">
+                                <img src="/storage/cat.jpg" class="card-img img-fluid img-thumb" alt="">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">*Nom de l'animal*</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet asperiores aspernatur at consequatur dolorum eaque illo ipsa laboriosam.</p>
+                                    <a href="#" class="card-link">Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="col-6">
             <h2 class="text-center mb-5">Vos animaux</h2>
@@ -29,6 +65,16 @@
             </div>
         </div>
     </div>
+
+
+    <div id='map' style='width: 400px; height: 300px;'></div>
+    <script>
+        mapboxgl.accessToken = 'pk.eyJ1Ijoid2FhenphIiwiYSI6ImNqeHVjdjlpNzAyZGIzbW9oOGJ1d292M2sifQ.Q8IMBCsYd3VsCfxGavM3AA';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        });
+    </script>
 
 
 
