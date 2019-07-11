c9577bc8c3ce908d8fea5fca21c0426984627df2
Marie <mariedugoua@gmail.com>
2019-07-11 11:23:16 +0200

    forms

--- resources/views/forms/animals/index.blade.php
+++ resources/views/forms/animals/index.blade.php
@@ -2,60 +2,91 @@
 @section('content')
     <form method="Post" enctype="multipart/form-data">
         @csrf
-        <div class="row">
-            <div class="col-6">
-                <div class="custom-file mt-3">
-                    <label class="custom-file-label" for="photo">Photo</label>
-                    <input type="file" class="custom-file-input" id="photo" name="photo" value="photo">
+        <div class="container formAnimal">
+            <div class="row">
+                <div class="col-6 picturInput">
+                    <label class="btn btn-block btn-primary img-fluid"> <input type="file" style="display: none;"></label>
+                        <div class="input-append date form_datetime">
+                            <input type="text" id="datepicker" readonly>
+                        </div>
+                </div>
+                <div class="col-6 categoryInput">
+                    <div class="form-group category" >
+                        <label for="name">Nom</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="form-group category" >
+                        <label for="type">Type</label>
+                        <select class="form-control" id="type" name="type">
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                        </select>
+                    </div>
+                    <div class="form-group category" >
+                        <label for="gender">Genre</label>
+                        <select class="form-control" id="gender" name="gender">
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                        </select>
+                    </div>
+                    <div class="form-group category">
+                        <label for="age">Age</label>
+                        <input type="range" class="form-control-range" id="age" name="age">
+                    </div>
+                    <div class="form-group category" >
+                        <label for="color">Couleur</label>
+                        <select class="form-control" id="color" name="color">
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                        </select>
+                    </div>
+                    <div class="form-group category">
+                        <select class="form-control addCategory" id="add" name="add">
+                            <option>Ajouter des caractéristique</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                        </select>
+                    </div>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="form-group" style="display:flex; justify-content:space-evenly;">
-                    <label for="name">Nom</label>
-                    <input type="text" class="form-control" id="name" name="name" style="width:30%;">
+            <div class="col-12 comment">
+                <div class="form-group">
+                    <label for="comment">Commentaire</label>
+                    <textarea class="form-control" id="comment" rows="3" name="comment"></textarea>
                 </div>
-                <div class="form-group" style="display:flex; justify-content:space-evenly;">
-                    <label for="type">Type</label>
-                    <select class="form-control" id="type" name="type" style="width:30%;">
-                        <option value="">1</option>
-                        <option value="">2</option>
-                        <option value="">3</option>
-                    </select>
+            </div>
+            <div class="row">
+                <div class="form-group form-check keepIt">
+                    <div class="col-4 checkHome">
+                        <input type="checkbox" class="form-check-input" id="home">
+                        <label class="form-check-label" for="home">Garder à la maison</label>
+                    </div>
+                    <div class="col-4 checkRefuge">
+                        <div class="refuge">
+                            <input type="checkbox" class="form-check-input" id="refuge">
+                            <label class="form-check-label" for="refuge">Je l'améne au refuge pour</label>
+                        </div>
+                        <div>
+                            <input type="number" class="day">
+                            <label for="jour">jour(s)</label>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div>
+                            <input type="number" class="address">
+                        </div>
+                        <div>
+                            <a href="">Trouver un refuge à proximité</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group" style="display:flex; justify-content:space-evenly;">
-                    <label for="gender">Genre</label>
-                    <select class="form-control" id="gender" name="gender" style="width:30%;">
-                        <option value="">1</option>
-                        <option value="">2</option>
-                        <option value="">3</option>
-                    </select>
-                </div>
-                <div class="form-group" style="display:flex; justify-content:space-evenly;">
-                    <label for="age">Age</label>
-                    <input type="range" class="form-control-range" id="age" name="age" style="width:30%;">
-                </div>
-                <div class="form-group" style="display:flex; justify-content:space-evenly;">
-                    <label for="color">Couleur</label>
-                    <select class="form-control" id="color" name="color" style="width:30%;">
-                        <option value="">1</option>
-                        <option value="">2</option>
-                        <option value="">3</option>
-                    </select>
-                </div>
-                <div class="form-group" style="display:flex; justify-content:space-evenly;">
-                    <select class="form-control" id="add" name="add">
-                        <option>Ajouter des caractéristique</option>
-                        <option value="">2</option>
-                        <option value="">3</option>
-                    </select>
-                </div>
+            </div>
+            <div>
+                <iframe src="https://www.google.com/maps/embed?pb=" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
         </div>
-       <div class="col-12">
-           <div class="form-group">
-               <label for="comment">Commentaire</label>
-               <textarea class="form-control" id="comment" rows="3" name="comment"></textarea>
-           </div>
-       </div>
     </form>
 @endsection
